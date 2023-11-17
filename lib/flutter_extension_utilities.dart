8f0edf8eadae6f8693504ff81e07238dcd766fb5
Raghav Joshi <developer.raghavjoshi@gmail.com>
2023-11-17 10:32:00 +0530

    add color extension

--- lib/flutter_extension_utilities.dart
+++ lib/flutter_extension_utilities.dart
@@ -1,5 +1,6 @@
 library flutter_extension_utilities;
 
 export 'package:extension_utilities/extension_utilities.dart';
+export 'package:flutter_extension_utilities/src/color.dart';
 export 'package:flutter_extension_utilities/src/media_query_context.dart';
 export 'package:flutter_extension_utilities/src/theme_context.dart';
